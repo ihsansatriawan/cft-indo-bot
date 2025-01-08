1170f5e289fecc5b7be931f6df1889b98672ab4b
Ihsan Satriawan <ihsansatriawan@Ihsans-Mac-mini.local>
2025-01-08 23:38:02 +0700

    feat(refactor): refactor index.js

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
-import 'dotenv/config'
-
-import TelegramBot from 'node-telegram-bot-api'
-import { storeData } from './src/store-data.js'
+import 'dotenv/config';
+import TelegramBot from 'node-telegram-bot-api';
+import { storeData } from './src/store-data.js';
 
 const token = process.env.TOKEN_TELEGRAM;
+const bot = new TelegramBot(token, { polling: true });
+const allowUserName = ['ihsansatriawan', 'mutirowahani'];
 
+const isUserAllowed = (userName) => allowUserName.includes(userName);
 
-const bot = new TelegramBot(token, {polling: true});
-
-const allowUserName = ['ihsansatriawan', 'mutirowahani']
-
-bot.on('message', async (msg) => {
+const handleIncomingMessage = async (msg) => {
   const chatId = msg.chat.id;
   const userName = msg.chat.username;
 
-	if (!allowUserName.includes(userName)) {
-		const errorMessage = `User ${userName} is not allowed to use this bot.`
-		await bot.sendMessage(chatId, errorMessage);
+  if (!isUserAllowed(userName)) {
+    const errorMessage = `User ${userName} is not allowed to use this bot.`;
+    await bot.sendMessage(chatId, errorMessage);
     return;
   }
 
-	const resultStore = await storeData(msg)
+  try {
+    const resultStore = await storeData(msg);
 
-
-  for (const result of resultStore) {
-    const { status, data } = result;
-		const dataParse = JSON.parse(data)
-    const { date, category, notes, source, price } = dataParse;
-    const message = `Received your message with Status: ${status}\nDate: ${date}\nCategory: ${category}\nNotes: ${notes}\nSource: ${source}\nPrice: ${price}`;
-    await bot.sendMessage(chatId, message);
+    for (const result of resultStore) {
+      const { status, data } = result;
+      const { date, category, notes, source, price } = JSON.parse(data);
+      const message = `Received your message with Status: ${status}\nDate: ${date}\nCategory: ${category}\nNotes: ${notes}\nSource: ${source}\nPrice: ${price}`;
+      await bot.sendMessage(chatId, message);
+    }
+  } catch (error) {
+    console.error('Error processing message:', error);
+    await bot.sendMessage(chatId, 'An error occurred while processing your message.');
   }
-});
+};
+
+bot.on('message', handleIncomingMessage);
